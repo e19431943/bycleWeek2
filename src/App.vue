e9ac5eca641378e6ac7952d22ff2130c9d6c9960
Nofly <e19431943@gmail.com>
2021-11-21 03:24:44 +0800

    完成首圖切版

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrep">
+    <div class="nav">
+      <h1 class="logo nav-item">
+        <div>
+          <img src="@/assets/images/svg/logo.svg" alt="logo">
+        </div>
+      </h1>
+      <router-link to="/" class="nav-item">
+        <div>
+          <img src="@/assets/images/svg/search.svg" alt="單車租借 ICON">
+        </div>
+      </router-link>
+      <router-link to="/" class="nav-item">
+        <div>
+          <img src="@/assets/images/svg/position.svg" alt="關鍵字搜尋 ICON">
+        </div>
+      </router-link>
+      <router-link to="/" class="nav-item">
+        <div>
+          <img src="@/assets/images/svg/bycle.svg" alt="自行車路線搜尋 ICON">
+        </div>
+      </router-link>
+      <router-link to="/" class="nav-item nav-last">
+        <div>
+          <img src="@/assets/images/svg/info.svg" alt="幫助中心 ICON">
+        </div>
+      </router-link>
+    </div>
+    <router-view class="main"/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import 'reset-css';
+@import '@/assets/scss/globel_variable.scss';
+
+.wrep {
+  width: 1440px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: $grey;
+  display: flex;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.nav {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: $light;
+  box-shadow:
+    1px 1px 10px 1px $light,
+    1px -1px 10px -1px #000
+  ;
+}
+.nav-item {
+  position: relative;
+  overflow: hidden;
+  width: 76px;
+  height: 76px;
+  & > div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    left: 0px;
+  }
+  img {
+    display: inline-block;
+  }
+  &:hover {
+    background-color: $secondary;
+    div {
+       left: -76px;
+      filter: drop-shadow($primary 76px 0);
     }
   }
 }
+.nav-last {
+  position: absolute;
+  bottom: 0;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="default-container">
+    <div class="enter-area">
+      <div class="enter-image mt-180">
+        <img alt="Enter Picture" src="../assets/images/svg/enter.svg">
+          <p>LET’SGO CYCLING!</p>
+      </div>
+      <div class="enter-post mt-75">
+        <p class="mb-36">身邊很多朋友都在有在騎自行車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車！</p>
+        <p class="mb-36">身邊很多朋友都在有在騎自行車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車，
+          常常看到他們一起出去騎車，常常看到他們一起出去騎車，常常看到他們一起出去騎車！</p>
+        <p class="mb-36">今天的你不管是要騎車去旅遊、上班、買菜甚至就單純想騎車，都希望你旅途平安，整天都開心：）</p>
+        <p class="mb-36">遇到任何問題都可以到    找我們，歡迎有任何建議也都可以告訴我們！</p>
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped lang="scss">
+@import '@/assets/scss/globel_variable.scss';
+@import '@/assets/scss/globel_public_style.scss';
+  .default-container {
+    width: 100%;
+    height: 100vh;
+    background-color: #bbb;
+  }
+  .enter-area {
+    box-sizing: border-box;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    padding-bottom: 75px;
+  }
+  .enter-image {
+    width: min-content;
+    height: min-content;
+    position: relative;
+    p {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      text-align: justify;
+      width: 121px;
+      color: $light;
+      font-size: 24px;
+      line-height: 35px;
+      letter-spacing: 1px;
+    }
+    &:hover > p {
+      color: $yellow;
+    }
+  }
+  .enter-post {
+    display: none;
+    width: 670px;
+    background-color: $light;
+    padding: 100px 50px 50px;
+    p {
+      font-size: 14px;
+      line-height: 20px;
+      letter-spacing: 5px;
+    }
+  }
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
